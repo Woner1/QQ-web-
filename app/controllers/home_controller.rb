e8c045ad2349c458756ec814b5d09815c1067510
Ankh <2609746800@qq.com>
2018-10-10 10:37:59 +0800

    mailer

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,11 @@
 class HomeController < ApplicationController
   def index
-    
+    UserMailer.account_activation(User.frist).deliver_now
   end
   def show
+    
+  end
+  def test
     
   end
 end
